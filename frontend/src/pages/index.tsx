5bb96b14d34653dc609123243e54ee1a3a0574a5
billy <aphexlog@gmail.com>
2023-06-03 19:39:22 -0500

    replacing react visual companants with tailwind

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,89 +1,59 @@
-import React, { useState } from "react"
-import type { PageProps } from "gatsby"
+import React, { useState } from "react";
+import { PageProps } from "gatsby";
+import "tailwindcss/tailwind.css";
 
-const containerStyles: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-  backgroundColor: "#F0F8FF",  // Light pastel blue
-  fontFamily: "'Roboto', sans-serif",
-}
-
-const chatContainerStyles: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  backgroundColor: 'white',
-  borderRadius: '10px',
-  padding: '20px',
-  width: '60%',
-  maxHeight: '500px',
-  overflowY: 'auto',
-  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', // Added box shadow
-}
-
-const messageStyles: React.CSSProperties = {
-  backgroundColor: '#0084FF',
-  color: 'white',
-  padding: '10px',
-  borderRadius: '10px',
-  margin: '10px 0',
-  alignSelf: 'flex-end',
-}
-
-const botMessageStyles: React.CSSProperties = {
-  ...messageStyles,
-  backgroundColor: '#E5E5E5',
-  color: 'black',
-  alignSelf: 'flex-start',
-}
-
-const formStyles: React.CSSProperties = {
-  display: "flex",
-  marginTop: "32px",
-  width: '60%',
-}
-
-const inputStyles: React.CSSProperties = {
-  flexGrow: "1",
-  marginRight: "16px",
-  borderRadius: '10px',
-  padding: '10px',
-}
+const containerStyles = "flex flex-col items-center justify-center h-screen bg-blue-200";
+const chatContainerStyles = "flex flex-col bg-white rounded-lg p-4 w-60 max-h-500 overflow-y-auto";
+const messageStyles = "bg-blue-500 text-white p-4 rounded-lg mb-4 self-end";
+const botMessageStyles = "bg-gray-200 text-black p-4 rounded-lg mb-4 self-start";
+const formStyles = "flex mt-8 w-60";
+const inputStyles = "flex-grow mr-4 rounded-lg p-4";
+const sendButtonStyles = "rounded-lg px-4 py-2 bg-blue-500 text-white font-bold transition-transform duration-200";
+const sendButtonPressedStyles = "transform scale-95";
 
 const IndexPage: React.FC<PageProps> = () => {
-  const [messages, setMessages] = useState<{message: string, user: string}[]>([])
-  const [chatInput, setChatInput] = useState<string>("")
+  const [messages, setMessages] = useState<{ message: string; user: string }[]>([]);
+  const [chatInput, setChatInput] = useState<string>("");
 
   function handleChatInputSubmit(event: React.FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
     if (chatInput) {
-      setMessages((oldMessages) => [...oldMessages, {message: chatInput, user: 'user'}])
-      setChatInput("")
+      setMessages((oldMessages) => [...oldMessages, { message: chatInput, user: "user" }]);
+      setChatInput("");
     }
   }
 
   return (
-    <main style={containerStyles}>
-      <div style={chatContainerStyles}>
+    <main className={containerStyles}>
+      <div className={chatContainerStyles}>
         {messages.map((messageObj, index) => (
-          <div key={index} style={messageObj.user === 'user' ? messageStyles : botMessageStyles}>{messageObj.message}</div>
+          <div
+            key={index}
+            className={messageObj.user === "user" ? messageStyles : botMessageStyles}
+          >
+            {messageObj.message}
+          </div>
         ))}
       </div>
 
-      <form onSubmit={handleChatInputSubmit} style={formStyles}>
+      <form onSubmit={handleChatInputSubmit} className={formStyles}>
         <input
           type="text"
           value={chatInput}
           onChange={(e) => setChatInput(e.target.value)}
-          style={inputStyles}
+          className={inputStyles}
         />
-        <input type="submit" value="Send" />
+        <button
+          type="submit"
+          className={sendButtonStyles}
+          onClick={() => console.log("Button pressed")} // Replace with your logic
+        >
+          Send
+        </button>
       </form>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
