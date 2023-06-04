87da2b9a006d3c4a6ce3fdd6467d1d1dd2e23e5d
billy <aphexlog@gmail.com>
2023-06-03 21:53:13 -0500

    made some visual improvements

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { PageProps } from "gatsby";
 
 const containerStyles = "flex flex-col items-center justify-center h-screen p-16 bg-blue-200";
-const chatContainerStyles = "bg-white rounded-lg p-4 w-full max-w-xl h-80 overflow-y-auto mb-8 shadow-lg";
+const chatContainerStyles = "bg-white rounded-lg p-7 w-full max-w-xl overflow-y-auto mb-8 shadow-lg";
 const messageContainerStyles = "flex justify-start";
 const messageStyles = "bg-blue-500 text-white p-4 rounded-lg mb-4 self-start transition-transform duration-300 transform hover:scale-105";
 const botMessageStyles = "bg-gray-200 text-black p-4 rounded-lg mb-4 self-start";
 const formStyles = "flex flex-col w-full max-w-xl";
 const inputStyles = "rounded-lg p-4 mb-4";
-const sendButtonStyles = "rounded-lg px-4 py-2 bg-blue-500 text-white font-bold transition-transform duration-200";
+const sendButtonStyles = "rounded-lg px-4 py-2 bg-blue-500 text-white font-bold transition-transform duration-200 transform";
 const sendButtonPressedStyles = "transform scale-95";
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -53,7 +53,7 @@ const IndexPage: React.FC<PageProps> = () => {
           className={sendButtonStyles}
           onClick={() => console.log("Button pressed")} // Replace with your logic
         >
-          Send
+          Enter
         </button>
       </form>
     </main>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: true,
+  darkMode: false,
   theme: {
     extend: {},
   },
