5fe9a59adf385b4ae353a6d3952e57e0f8fb4e5c
Aaron West <aphexlog@gmail.com>
2024-06-27 20:36:36 -0500

    adding damage & defense stats

--- characters/inventory.py
+++ characters/inventory.py
@@ -13,10 +13,18 @@ class PlayerInventory:
             json.dump(self.inventory, f, indent=4)
         print(f"Inventory saved to {filename}")
 
-    def add_weapon(self, weapon_name):
-        self.inventory["weapons"].append(weapon_name)
-        self._save_inventory_to_json("player_inventory.json")  # Save after adding weapon
+    def add_weapon(self, weapon_name, damage):
+        weapon = {
+            "name": weapon_name,
+            "damage": damage
+        }
+        self.inventory["weapons"].append(weapon)
+        self._save_inventory_to_json("player_inventory.json")
 
-    def add_armor(self, armor_name):
-        self.inventory["armor"].append(armor_name)
-        self._save_inventory_to_json("player_inventory.json")  # Save after adding armor
+    def add_armor(self, armor_name, defense):
+        armor = {
+            "name": armor_name,
+            "defense": defense
+        }
+        self.inventory["armor"].append(armor)
+        self._save_inventory_to_json("player_inventory.json")
