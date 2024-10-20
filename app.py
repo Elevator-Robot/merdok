c1dc31a6a88d6d343c64d77f563c3efd1750c2fa
Aaron West <aphexlog@gmail.com>
2024-10-20 15:52:03 -0500

    adding graphql schema

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from aws_cdk import (
     Stack,
     aws_bedrock as bedrock,
     aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_dynamodb as dynamodb,
+    aws_appsync as appsync,
 )
 from constructs import Construct
 from aws_cdk.aws_iam import IPrincipal
@@ -13,6 +16,20 @@ class MerdokStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # DynamoDB Table for Chat Sessions
+        chat_table = dynamodb.Table(self, "ChatTable",
+            partition_key=dynamodb.Attribute(
+                name="chatSessionId",
+                type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name="timestamp",
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+
+        # IAM Role for Bedrock Agent
         bedrock_agent_service_role = iam.Role(self, "BedrockAgentServiceRole",
             assumed_by=cast(IPrincipal, iam.ServicePrincipal("bedrock.amazonaws.com"))
         )
@@ -27,16 +44,10 @@ class MerdokStack(Stack):
             ]
         ))
 
+        # Bedrock Agent Definition
         bedrock.CfnAgent(self, "BedrockAgent",
             agent_name="MerdokAgent",
             description="Merdok Agent",
-            action_groups=[
-                bedrock.CfnAgent.AgentActionGroupProperty(
-                    action_group_name="bedrock-dice-roller",
-                    action_group_state="ENABLED",
-                    description="Default action group",
-                )
-            ],
             foundation_model=bedrock.FoundationModel.from_foundation_model_id(
                 self, "FoundationModel",
                 bedrock.FoundationModelIdentifier.ANTHROPIC_CLAUDE_3_5_SONNET_20240620_V1_0,
@@ -52,6 +63,65 @@ class MerdokStack(Stack):
             idle_session_ttl_in_seconds=300,
             agent_resource_role_arn=bedrock_agent_service_role.role_arn,
         )
+
+        # Lambda Function to Interact with Bedrock
+        bedrock_lambda = _lambda.Function(self, "BedrockLambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="bedrock_handler.lambda_handler",
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                "CHAT_TABLE_NAME": chat_table.table_name
+            }
+        )
+
+        # Grant DynamoDB Permissions to Lambda
+        chat_table.grant_read_write_data(bedrock_lambda)
+
+        # AppSync API Definition
+        api = appsync.CfnGraphQLApi(self, "MerdokApi",
+            name="MerdokApi",
+            authentication_type="AMAZON_COGNITO_USER_POOLS",
+            xray_enabled=True,
+            user_pool_config=appsync.CfnGraphQLApi.UserPoolConfigProperty(
+                user_pool_id="us-east-1_jq7L9L7CH",
+                aws_region="us-east-1",
+                default_action="ALLOW"
+            )
+        )
+
+        # Output the value of api.ref
+        cdk.CfnOutput(self, "GraphQLApiId",
+            value=api.attr_api_id,
+            description="The ID of the AppSync GraphQL API"
+        )
+        # GraphQL Schema
+        appsync.CfnGraphQLSchema(self, "MerdokSchema",
+            api_id=api.attr_api_id,
+            definition=open("schema.graphql").read()
+        )
+
+        # Lambda Data Source for AppSync
+        lambda_data_source = appsync.CfnDataSource(self, "LambdaDataSource",
+            api_id=api.attr_api_id,
+            name="LambdaDataSource",
+            type="AWS_LAMBDA",
+            lambda_config=appsync.CfnDataSource.LambdaConfigProperty(
+                lambda_function_arn=bedrock_lambda.function_arn
+            ),
+            service_role_arn=bedrock_agent_service_role.role_arn
+        )
+
+        # Example Resolver for sendMessageToChat
+        resolver = appsync.CfnResolver(self, "SendMessageToChatResolver",
+            api_id=api.attr_api_id,
+            type_name="Mutation",
+            field_name="sendMessageToChat",
+            data_source_name=lambda_data_source.name,
+            request_mapping_template=open("resolvers/sendMessageToChatRequest.vtl").read(),
+            response_mapping_template=open("resolvers/sendMessageToChatResponse.vtl").read(),
+        )
+
+        resolver.node.add_dependency(lambda_data_source)
 
 app = cdk.App()
 MerdokStack(app, "MerdokStack")
