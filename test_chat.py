6c888a14aed94ebe201905511ef4dbace6fd6456
Aaron West <aphexlog@gmail.com>
2024-11-02 14:09:48 -0500

    fix: Improve formatting and logging in get_api_details function

--- test_chat.py
+++ test_chat.py
@@ -4,26 +4,29 @@ import json
 import os
 from datetime import datetime
 
+
 # Get the API details from AWS
 def get_api_details():
-    client = boto3.client('appsync')
+    client = boto3.client("appsync")
     # List GraphQL APIs
     response = client.list_graphql_apis()
-    
+
     # Get the first API (assuming it's our chat API)
-    api = response['graphqlApis'][0]
-    api_id = api['apiId']
-    api_url = api['uris']['GRAPHQL']
-    
+    api = response["graphqlApis"][0]
+    print(f"API: {api}")
+    api_id = api["apiId"]
+    api_url = api["uris"]["GRAPHQL"]
+
     # Get API Key
     keys = client.list_api_keys(apiId=api_id)
-    api_key = keys['apiKeys'][0]['id']
-    
+    api_key = keys["apiKeys"][0]["id"]
+
     return api_url, api_key
+
 
 def send_test_message():
     api_url, api_key = get_api_details()
-    
+
     # GraphQL mutation
     mutation = """
     mutation SendMessage {
@@ -41,22 +44,16 @@ def send_test_message():
         }
     }
     """
-    
+
     # Headers for the request
-    headers = {
-        'Content-Type': 'application/json',
-        'x-api-key': api_key
-    }
-    
+    headers = {"Content-Type": "application/json", "x-api-key": api_key}
+
     # Make the request
-    response = requests.post(
-        api_url,
-        headers=headers,
-        json={'query': mutation}
-    )
-    
+    response = requests.post(api_url, headers=headers, json={"query": mutation})
+
     # Print the response
     print(json.dumps(response.json(), indent=2))
+
 
 if __name__ == "__main__":
     send_test_message()
